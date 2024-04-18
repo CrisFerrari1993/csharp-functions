5993d26d65065276cf7cc4d6213106e892450be0
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-18 15:37:23 +0200

    Bonus work in progress

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,10 +1,80 @@
-﻿namespace csharp_functions
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_functions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+           int[] numeri = { 2, 6, 7, 5, 3, 9 };
+            //Una volta completate queste funzioni di utilità di base, e dato il seguente array di numeri [2, 6, 7, 5, 3, 9] già dichiarato nel vostro codice, si vogliono utilizzare le funzioni per:
+            //- Stampare l’array di numeri fornito a video
+            StampaArray(numeri);
+            //- Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato (Verificare che l’array originale non sia stato modificato quindi ristampare nuovamente l’array originale e verificare che sia rimasto [2, 6, 7, 5, 3, 9])
+            Console.WriteLine("Array che ha i numeri elevati al quadrato");
+            ElevaArrayAlQuadrato(numeri);
+            Console.WriteLine("Array originale");
+            StampaArray(numeri);
+            //- Stampare la somma di tutti i numeri
+            Console.WriteLine("La somma di tutti gli elementi dell'array è: " + SommaElementiArray(numeri));
+            //- Stampare la somma di tutti i numeri elevati al quadrati
+            int[] copia = ElevaArrayAlQuadrato(numeri);
+            Console.WriteLine("La somma di tutti gli elementi dell'array elevati a potenza è: " + SommaElementiArray(copia));
+            //**BONUS:** Convertire le funzioni appena dichiarate in funzioni generiche, ossia funzioni che possono lavorare con array di numeri interi **di lunghezza variabile**, ossia debbono poter funzionare sia che gli passi array di 5 elementi, sia di 6, di 7, di ... e così via.
+            //A questo punto modificare il programma in modo che chieda all’utente quanti numeri voglia inserire, e dopo di che questi vengono inseriti a mano dall’utente esternamente. Rieseguire il programma con l’input preso esternamente dall’utente.
+        }
+        //- **void StampaArray(int[] array)**: che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. Potete prendere quella fatta in classe questa mattina
+        static void StampaArray(int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == array.Length - 1)
+                {
+                    Console.Write($"{array[i]}");
+                }
+                else
+                {
+                    Console.Write($"{array[i]}, ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+        /*
+        -**int Quadrato(int numero)**: che vi restituisca il quadrato del numero passato come parametro.
+        */
+        static int Quadrato(int numero)
+        {
+            return numero * numero;
+        }
+        /*
+             - **int[] ElevaArrayAlQuadrato(int[] array)**: che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
+            */
+        static int[] ElevaArrayAlQuadrato(int[] array)
+        {
+            int[] copiaArr = (int[])array.Clone();
+            int index = 0;
+            foreach (int n in array)
+            {
+                copiaArr[index] = Quadrato(n);
+                index++;
+            }
+            StampaArray(copiaArr);
+            return copiaArr;
+
+
+        }
+        /*
+        -**int sommaElementiArray(int[] array)**: che preso un array di numeri interi, restituisca la somma totale di tutti gli elementi dell’array.
+        */
+        static int SommaElementiArray(int[] array)
+        {
+            int sum = 0;
+            foreach (int n in array)
+            {
+                sum += n;
+            }
+            return sum;
         }
     }
 }
